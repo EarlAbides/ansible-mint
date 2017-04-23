b6598f49f6b0c5df3785e910b97ec9cc590bfa3f
Jeffrey Earl <jeffreyearl@hotmail.com>
2017-04-23 10:31:10 -0400

    WIP - Add docker, kernel upgrade fragged Cinnamon.

--- ansible-bootstrap.sh
+++ ansible-bootstrap.sh
@@ -3,3 +3,11 @@
 sudo apt install -y python-pip python-setuptools python-wheel
 sudo -H pip install --upgrade pip
 sudo -H pip install ansible
+
+# Install docker pre-reqs. Will require reboot before
+# running the playbook
+sudo apt install -y linux-image-generic linux-image-extra-virtual
+
+echo
+echo "*** IMPORTANT ***"
+echo "System reboot required prior to executing run-user.sh"
